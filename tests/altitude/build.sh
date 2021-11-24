b4fbb37528320f7cba04b730a9377ca2cb0bd5f0
root <root@rain.home>
2021-11-24 02:09:02 -0500

    Altidtude test example with library
    A basic altitude library for working with the BMP180 barometer
    The library mainly abstracts from the sensor details and data aquasition
    
    The function of this library to the ROM-623 is to identify the aircrafts altitude which
    this library achives with an altitude conversion based in the relative pressure

---
+++ tests/altitude/build.sh
@@ -0,0 +1,10 @@
+board="uno"
+device_path="/dev/ttyACM0"
+board_type="arduino:avr:$board"
+
+arduino-cli compile -b $board_type -p $device_path
+echo -e "compiled\n"
+
+arduino-cli upload altitude.ino -b $board_type -p $device_path
+echo -e "uploaded\n"
+
