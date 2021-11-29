11fe9a4d7279ea4739d614cbd2789865898184fa
root <root@rain.home>
2021-11-28 23:35:24 -0500

    Added shceduler feasibility check
    Allows to check the feasibility weater a set of tasks is schedulable or not before attempting to schedule them
    
    One check method currently is implemented and is based on cpu utilization

--- src/scheduler.c
+++ src/scheduler.c
@@ -9,6 +9,14 @@ struct Job schedule[MAX_SIZE];
 int task_index = 0;
 struct Job tasks[MAX_TASKS];
 
+void zeroTasks() {
+    int i = 0;
+    while(i < MAX_TASKS) {
+        tasks[i] = (struct Job){0};
+        i += 1;
+    }
+}
+
 int findTask(uint8_t id) {
     int i = 0;
     while (i < MAX_TASKS) {
@@ -45,6 +53,42 @@ void printTaskList() {
         printf("Task id: %d\n", tasks[i].ID);
         i += 1;
     }
+}
+
+
+float cpuUtil() {
+    float cpu_util = 0;
+
+    int i = 0;
+    while(i < MAX_TASKS) {
+        if(tasks[i].ID == 0) {
+        } 
+        else if(tasks[i].t_info.period == 0 || tasks[i].t_info.exec_time == 0) {
+            return 1; //Task list contains impossible tasks
+        } 
+        else{
+            float period = tasks[i].t_info.period;
+            float exec_t = tasks[i].t_info.exec_time;
+            cpu_util += (exec_t/period);
+        }
+        i += 1;
+    }
+
+    return cpu_util;
+}
+
+int schedulerFeasibilty() {
+    float cpu_util = cpuUtil();
+
+    if(cpu_util >= 1) {
+        printf("CPU_UTIL: %f\n", cpu_util);
+        perror("ERROR: cpu_util is > 1");
+        return -1;
+    } else {
+        return 1;
+    }
+
+    //check other cases
 }
 
 void printTask(const struct Job task) {
--- tests/new_tasks.c
+++ tests/new_tasks.c
@@ -1,24 +1,34 @@
 #include "../src/scheduler.c"
 
+//
 
 int main() {
-const struct TimeInfo t_info1 = createTInfo(2, 10, 4, 5, 1, 0);
+    //Exec, prio, period, deadl, dead_t, realease
+const struct TimeInfo t_info1 = createTInfo(1, 10, 2, 5, 1, 0);
+
+    //ptr, ptr, interuptable, func_type
 const struct ExecConstraints ex_con1 = createExCons(NULL, NULL, 0, 2);
+
+
 const struct Job job1 = createJob(ex_con1, t_info1, 1);
 
-const struct TimeInfo t_info2 = createTInfo(2, 10, 4, 5, 1, 0);
+const struct TimeInfo t_info2 = createTInfo(1, 10, 2, 5, 1, 0);
 const struct ExecConstraints ex_con2 = createExCons(NULL, NULL, 0, 2);
-const struct Job job2 = createJob(ex_con2, t_info2, 3);
+const struct Job job2 = createJob(ex_con2, t_info2, 6);
 
-const struct Job job3 = createJob(ex_con2, t_info2, 8);
+const struct TimeInfo t_info3 = createTInfo(1, 10, 2, 5, 1, 0);
+const struct ExecConstraints ex_con3 = createExCons(NULL, NULL, 0, 2);
+const struct Job job3 = createJob(ex_con3, t_info3, 3);
 
+zeroTasks();
 addTask(job1);
 addTask(job2);
 addTask(job3);
 
+//removeTask(job2.ID);
 printTaskList();
-removeTask(job2.ID);
-printTaskList();
+
+printf("Is feasible: %d\n", schedulerFeasibilty());
 
 printf("find task id %d\nIndex: %d\n", job3.ID, findTask(job3.ID));
 }
