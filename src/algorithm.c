4ccd8beb8cd54dbc0473e9179e5436bd8e7d7d28
root <root@rain.home>
2021-11-29 21:50:12 -0500

    Reverted changes

--- src/algorithm.c
+++ src/algorithm.c
@@ -14,79 +14,46 @@ void printJobs() {
 }
 
 void fullSchedule() {
-	algorithm_full();
+    algorithm_full();
 }
 
 void scheduleNext() {
-	algorithm_next();
+    algorithm_next();
 }
 
 int findAvailableTime(struct Job* job_ptr) {
 }
 
 int addQue() {
-	int i = 0;
-	while (i < MAX_QUE) {
-		if (task_index == MAX_QUE) {
-			return -1;
-		}
-		else if (que[i].ID == 0) {}
-		else {
-			addTask(que[i]);
-			que[i] = (struct Job){0};
-		}
-		i += 1;
-	}
-	return 1;
+    int i = 0;
+    while (i < MAX_QUE) {
+        if (task_index == MAX_QUE) {
+            return -1;
+        }
+        else if (que[i].ID == 0) {}
+        else {
+            addTask(que[i]);
+            que[i] = (struct Job){0};
+        }
+        i += 1;
+    }
+    return 1;
 }
-
-
-void periodsort() {
-
-	struct Job holder;
-	
-	while (i < MAX_TASKS) {
-
-		if (tasks[i].ID == 0){
-			continue;
-		}
-		else {
-			
-			for(int i = 0: i < MAX_TASKS: i++){
-				
-				for(int k = 0: k < MAX_TASKS: k++){
-					
-					if(task[i].t_info.period > task[k].t_info.period){
-						continue;
-					} 
-					else{
-						holder = task[i];
-						task[i] = task[k];
-						task[k] = holder;
-					}
-				}
-			}
-		}		
-
-	}
-
-}
-
 
 void fcfcFull() {
-	int i = 0;
-	while (i < MAX_TASKS) {
-		if(tasks[i].ID == 0) {
-		}
-		else {
-			schedule[i] = &tasks[i];
-		}
+    int i = 0;
+    while (i < MAX_TASKS) {
+        if(tasks[i].ID == 0) {
+        }
+        else {
+            schedule[i] = &tasks[i];
+        }
 
-		i += 1;
-	}
+        i += 1;
+    }
 }
 
 void fcfcNext() {
-	addQue();
-	fcfcFull();
+    addQue();
+    fcfcFull();
 }
